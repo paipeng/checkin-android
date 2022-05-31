2f3c99ed5d082ed970843bb633ed9de16f312d05
Pai Peng <sipaipv6@gmail.com>
2022-05-31 13:39:08 +0800

    add sm4 and unit test

---
+++ app/src/test/java/com/paipeng/checkin/utils/SM4UtilTest.java
@@ -0,0 +1,71 @@
+package com.paipeng.checkin.utils;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+public class SM4UtilTest extends TestCase {
+
+    public void testGenerateKey() throws Exception {
+        byte[] key = SM4Util.generateKey();
+        Assert.assertNotNull(key);
+        System.out.println("key size: " + key.length);
+        for (int i = 0; i < key.length; i++) {
+            System.out.print(Integer.toHexString(key[i] & 0xFF) + ((i<(key.length-1))?"-":""));
+        }
+        System.out.println("\n");
+    }
+
+    public void testGenerateKeySize8() throws Exception {
+        byte[] key = SM4Util.generateKey(8*8);
+        Assert.assertNotNull(key);
+        System.out.println("key size: " + key.length);
+        for (int i = 0; i < key.length; i++) {
+            System.out.print(Integer.toHexString(key[i] & 0xFF) + ((i<(key.length-1))?"-":""));
+        }
+        System.out.println("\n");
+    }
+
+    public void testGenerateKeySize16() throws Exception {
+        byte[] key = SM4Util.generateKey(16*8); // bits
+        Assert.assertNotNull(key);
+        System.out.println("key size: " + key.length);
+        for (int i = 0; i < key.length; i++) {
+            System.out.print(Integer.toHexString(key[i] & 0xFF) + ((i<(key.length-1))?"-":""));
+
+        }
+        System.out.println("\n");
+    }
+
+    public void testEncryptEcb() throws Exception {
+    }
+
+    public void testEncrypt_Ecb_Padding() throws Exception {
+        byte[] key = SM4Util.generateKey(16*8); // bits
+        int data_len = 16;
+        byte[] input = new byte[data_len];
+        for (int i = 0; i < data_len; i++) {
+            input[i] = (byte)i;
+        }
+
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(Integer.toHexString(input[i] & 0xFF) + ((i<(input.length-1))?"-":""));
+        }
+        System.out.println("\n");
+
+
+        byte[] encoded = SM4Util.encrypt_Ecb_Padding(key, input);
+        for (int i = 0; i < encoded.length; i++) {
+            System.out.print(Integer.toHexString(encoded[i] & 0xFF) + ((i<(encoded.length-1))?"-":""));
+        }
+        System.out.println("\n");
+
+
+        byte[] decoded = SM4Util.decrypt_Ecb_Padding(key, encoded);
+        for (int i = 0; i < decoded.length; i++) {
+            System.out.print(Integer.toHexString(decoded[i] & 0xFF) + ((i<(decoded.length-1))?"-":""));
+            Assert.assertEquals(input[i], decoded[i]);
+        }
+        System.out.println("\n");
+    }
+}
