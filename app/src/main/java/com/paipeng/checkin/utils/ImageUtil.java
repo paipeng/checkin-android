f09e87d69f23fa2d2b9f0168ab19ab1200ecc64f
Pai Peng <sipaipv6@gmail.com>
2022-05-31 12:00:48 +0800

    add image functions

--- app/src/main/java/com/paipeng/checkin/utils/ImageUtil.java
+++ app/src/main/java/com/paipeng/checkin/utils/ImageUtil.java
@@ -3,8 +3,13 @@ package com.paipeng.checkin.utils;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
+import android.media.Image;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
 
 public class ImageUtil {
+    private static final String TAG = ImageUtil.class.getSimpleName();
 
     public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
 
@@ -48,5 +53,44 @@ public class ImageUtil {
             e.printStackTrace();
             return null;
         }
+    }
+
+
+    public static Bitmap resize(Bitmap bitmap, int newWidth, int newHeight) {
+        if (bitmap == null || bitmap.isRecycled()) {
+            return null;
+        }
+        try {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            float scaleWidth = ((float) newWidth) / width;
+            float scaleHeight = ((float) newHeight) / height;
+            Matrix matrix = new Matrix();
+            matrix.postScale(scaleWidth, scaleHeight);
+
+            return Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+        } catch (Exception | Error exception) {
+            Log.e(TAG, "resize " + exception.getMessage());
+            return null;
+        }
+    }
+
+    public static byte[] bitmapToRGBByteArray(Bitmap bitmap) {
+        int bytes = bitmap.getByteCount();
+        ByteBuffer buffer = ByteBuffer.allocate(bytes);
+        bitmap.copyPixelsToBuffer(buffer);
+
+        byte[] rgba = buffer.array();
+        byte[] pixels = new byte[(rgba.length / 4) * 3];
+
+        int count = rgba.length / 4;
+        for (int i = 0; i < count; i++) {
+
+            pixels[i * 3] = rgba[i * 4];        //R
+            pixels[i * 3 + 1] = rgba[i * 4 + 1];    //G
+            pixels[i * 3 + 2] = rgba[i * 4 + 2];       //B
+
+        }
+        return pixels;
     }
 }
