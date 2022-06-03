7f3e23a058346ac6e7462a406e2489b9a2ed7d68
Pai Peng <sipaipv6@gmail.com>
2022-06-03 08:05:01 +0800

    add attribute bitmaps

--- app/src/main/java/com/paipeng/checkin/model/FaceOCRIdCard.java
+++ app/src/main/java/com/paipeng/checkin/model/FaceOCRIdCard.java
@@ -9,4 +9,71 @@ import java.util.List;
 public class FaceOCRIdCard extends IdCard{
     private Bitmap capturedBitmap;
     private List<OcrResultModel> ocrResultModels;
+    private Bitmap chipUIDBitmap;
+    private Bitmap expireDateBitmap;
+    private Bitmap serialNumberBitmap;
+    private Bitmap companyBitmap;
+    private Bitmap nameBitmap;
+
+    public Bitmap getCapturedBitmap() {
+        return capturedBitmap;
+    }
+
+    public void setCapturedBitmap(Bitmap capturedBitmap) {
+        this.capturedBitmap = capturedBitmap;
+    }
+
+    public List<OcrResultModel> getOcrResultModels() {
+        return ocrResultModels;
+    }
+
+    public void setOcrResultModels(List<OcrResultModel> ocrResultModels) {
+        this.ocrResultModels = ocrResultModels;
+    }
+
+    public Bitmap getChipUIDBitmap() {
+        return chipUIDBitmap;
+    }
+
+    public void setChipUIDBitmap(Bitmap chipUIDBitmap) {
+        this.chipUIDBitmap = chipUIDBitmap;
+    }
+
+
+    public Bitmap getExpireDateBitmap() {
+        return expireDateBitmap;
+    }
+
+    public void setExpireDateBitmap(Bitmap expireDateBitmap) {
+        this.expireDateBitmap = expireDateBitmap;
+    }
+
+    public Bitmap getSerialNumberBitmap() {
+        return serialNumberBitmap;
+    }
+
+    public void setSerialNumberBitmap(Bitmap serialNumberBitmap) {
+        this.serialNumberBitmap = serialNumberBitmap;
+    }
+
+    public Bitmap getCompanyBitmap() {
+        return companyBitmap;
+    }
+
+    public void setCompanyBitmap(Bitmap companyBitmap) {
+        this.companyBitmap = companyBitmap;
+    }
+
+    public Bitmap getNameBitmap() {
+        return nameBitmap;
+    }
+
+    public void setNameBitmap(Bitmap nameBitmap) {
+        this.nameBitmap = nameBitmap;
+    }
+
+    public boolean isValidate() {
+        return this.nameBitmap != null && this.companyBitmap != null && this.serialNumberBitmap != null && this.expireDateBitmap != null && this.chipUIDBitmap != null;
+    }
+
 }
